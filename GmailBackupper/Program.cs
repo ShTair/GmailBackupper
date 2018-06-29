4fa70c990676e11401889562c03a234389dc8e29
ShTair <git@vsh.jp>
2018-06-29 17:35:37 +0900

    チャットも保存するように変更

--- GmailBackupper/Program.cs
+++ GmailBackupper/Program.cs
@@ -51,52 +51,49 @@ namespace GmailBackupper
 
                 Console.Write(message.id);
 
-                if (message.labelIds.All(t => t != "CHAT"))
+                var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_jp);
+                Console.Write(time.ToString(" yyyy-MM-dd HH:mm:ss"));
+
+                var pathJ = Path.Combine(bupath, "json", message.id.Substring(14), message.id.Substring(12));
+                Directory.CreateDirectory(pathJ);
+                var fileJ = Path.Combine(pathJ, message.id + ".json");
+
+                var pathE = Path.Combine(bupath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
+                Directory.CreateDirectory(pathE);
+                var fileE = Path.Combine(pathE, message.id + ".eml");
+
+                if (!File.Exists(fileJ))
                 {
-                    var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_jp);
-                    Console.Write(time.ToString(" yyyy-MM-dd HH:mm:ss"));
-
-                    var pathJ = Path.Combine(bupath, "json", message.id.Substring(14), message.id.Substring(12));
-                    Directory.CreateDirectory(pathJ);
-                    var fileJ = Path.Combine(pathJ, message.id + ".json");
-
-                    var pathE = Path.Combine(bupath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
-                    Directory.CreateDirectory(pathE);
-                    var fileE = Path.Combine(pathE, message.id + ".eml");
-
-                    if (!File.Exists(fileJ))
+                    using (var stream = File.Create(tempFile))
                     {
-                        using (var stream = File.Create(tempFile))
+                        await gmail.GetMessageStr(mid.Id, async src =>
                         {
-                            await gmail.GetMessageStr(mid.Id, async src =>
-                            {
-                                await src.CopyToAsync(stream);
-                            });
-                        }
-
-                        message = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Raw);
-                        var str = _regex.Replace(message.raw, m => m.Value == "-" ? "+" : "/");
-                        var raw = Convert.FromBase64String(str);
-                        await File.WriteAllBytesAsync(fileE, raw);
-                        File.SetCreationTime(fileE, time.DateTime);
-                        File.SetLastWriteTime(fileE, time.DateTime);
-
-                        File.Move(tempFile, fileJ);
-                        File.SetCreationTime(fileJ, time.DateTime);
-                        File.SetLastWriteTime(fileJ, time.DateTime);
-
-                        Console.Write(" Store");
+                            await src.CopyToAsync(stream);
+                        });
                     }
 
-                    if (message.labelIds.Any(t => t == "INBOX"))
+                    message = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Raw);
+                    var str = _regex.Replace(message.raw, m => m.Value == "-" ? "+" : "/");
+                    var raw = Convert.FromBase64String(str);
+                    await File.WriteAllBytesAsync(fileE, raw);
+                    File.SetCreationTime(fileE, time.DateTime);
+                    File.SetLastWriteTime(fileE, time.DateTime);
+
+                    File.Move(tempFile, fileJ);
+                    File.SetCreationTime(fileJ, time.DateTime);
+                    File.SetLastWriteTime(fileJ, time.DateTime);
+
+                    Console.Write(" Store");
+                }
+
+                if (message.labelIds.Any(t => t == "INBOX"))
+                {
+                    if (message.labelIds.All(t => t != "STARRED"))
                     {
-                        if (message.labelIds.All(t => t != "STARRED"))
+                        if (time < limit)
                         {
-                            if (time < limit)
-                            {
-                                await gmail.MoveToTrash(mid.Id);
-                                Console.Write(" Trash");
-                            }
+                            await gmail.MoveToTrash(mid.Id);
+                            Console.Write(" Trash");
                         }
                     }
                 }
