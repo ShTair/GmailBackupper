137c66db4ae8c3493ba444890952c303514456e9
ShTair <git@vsh.jp>
2018-06-30 23:01:18 +0900

    リファクタリング

--- GmailBackupper/Program.cs
+++ GmailBackupper/Program.cs
@@ -10,10 +10,10 @@ namespace GmailBackupper
 {
     class Program
     {
-        private static readonly Regex _regex = new Regex(@"[-_]");
-        private static readonly Regex _fromRegex = new Regex(@"""?(.*?)""?\s*<(.+)>");
-        private static readonly Regex _fileRegex = new Regex(@"[\\/:*?""<>|\r\n\t]");
-        private static readonly TimeSpan _jp = TimeSpan.FromHours(9);
+        private static readonly Regex _base64UrlRegex = new Regex(@"[-_]");
+        private static readonly Regex _mailHeaderFromRegex = new Regex(@"""?(.*?)""?\s*<(.+)>");
+        private static readonly Regex _fileNameRegex = new Regex(@"[\\/:*?""<>|\r\n\t]");
+        private static readonly TimeSpan _japanTimeSpan = TimeSpan.FromHours(9);
 
         static void Main(string[] args)
         {
@@ -22,29 +22,26 @@ namespace GmailBackupper
 
         private static async Task Run(string settingsPath)
         {
-            var settingsJson = await File.ReadAllTextAsync(settingsPath);
-            var gs = JsonConvert.DeserializeObject<GlobalSettings>(settingsJson);
+            var gSettingsJson = await File.ReadAllTextAsync(settingsPath);
+            var gSettings = JsonConvert.DeserializeObject<GlobalSettings>(gSettingsJson);
 
-            foreach (var acsp in Directory.EnumerateFiles(gs.SettingsPath, "*.json"))
+            foreach (var aSettingsPath in Directory.EnumerateFiles(gSettings.SettingsPath, "*.json"))
             {
                 try
                 {
-                    var acsj = await File.ReadAllTextAsync(acsp);
-                    var acs = JsonConvert.DeserializeObject<AccountSettings>(acsj);
-                    await Run(gs.ClientId, gs.ClientSecret, acs.RefreshToken, acs.BackupPath, acs.Limit);
+                    var aSettingsJson = await File.ReadAllTextAsync(aSettingsPath);
+                    var aSettings = JsonConvert.DeserializeObject<AccountSettings>(aSettingsJson);
+                    await Run(gSettings.ClientId, gSettings.ClientSecret, aSettings.RefreshToken, aSettings.BackupPath, aSettings.Limit);
                 }
                 catch (Exception exp) { Console.WriteLine(exp.ToString()); }
             }
         }
 
-        private static async Task Run(string cid, string cs, string rt, string bupath, int limitday)
+        private static async Task Run(string clientId, string clientSecret, string refreshToken, string dstPath, int limitDays)
         {
-            var _jp = TimeSpan.FromHours(9);
-            var gmail = new Gmail(cid, cs, rt);
+            var gmail = new Gmail(clientId, clientSecret, refreshToken);
             var messages = gmail.GetMessageEnamerator();
-            var tempFile = Path.Combine(bupath, "temp.json");
-
-            var limit = DateTimeOffset.Now.AddDays(-limitday);
+            var limit = DateTimeOffset.Now.AddDays(-limitDays);
 
             while (true)
             {
@@ -55,13 +52,13 @@ namespace GmailBackupper
                 {
                     Console.Write(mid.Id);
 
-                    var pathJ = Path.Combine(bupath, "json", mid.Id.Substring(14), mid.Id.Substring(12));
-                    var fileJ = Path.Combine(pathJ, mid.Id + ".json");
+                    var jsonPath = Path.Combine(dstPath, "json", mid.Id.Substring(14), mid.Id.Substring(12));
+                    var jsonName = Path.Combine(jsonPath, mid.Id + ".json");
 
-                    if (!File.Exists(fileJ))
+                    if (!File.Exists(jsonName))
                     {
-                        Directory.CreateDirectory(pathJ);
-                        using (var stream = File.Create(fileJ))
+                        Directory.CreateDirectory(jsonPath);
+                        using (var stream = File.Create(jsonName))
                         {
                             await gmail.GetMessageStr(mid.Id, async src =>
                             {
@@ -70,23 +67,23 @@ namespace GmailBackupper
                         }
                     }
 
-                    var json = await File.ReadAllTextAsync(fileJ);
+                    var json = await File.ReadAllTextAsync(jsonName);
                     var message = JsonConvert.DeserializeObject<MessageResultModel>(json);
                     if (message == null || message.internalDate == 0)
                     {
-                        File.Delete(fileJ);
+                        File.Delete(jsonName);
                         Console.WriteLine();
                         continue;
                     }
 
-                    var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_jp);
+                    var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_japanTimeSpan);
                     Console.Write($" {time:yyyy-MM-dd HH:mm:ss}");
 
                     var fn = $"{time:yyyyMMddHHmmss}_{message.id}";
                     var from = message.payload.headers.FirstOrDefault(t => t.name.Equals("From", StringComparison.CurrentCultureIgnoreCase));
                     if (from != null)
                     {
-                        var m = _fromRegex.Match(from.value);
+                        var m = _mailHeaderFromRegex.Match(from.value);
                         if (!m.Success)
                         {
                             fn += "_" + from.value.Trim(20);
@@ -103,22 +100,22 @@ namespace GmailBackupper
                         fn += "_" + subj.value.Trim(20);
                     }
 
-                    fn = _fileRegex.Replace(fn, "");
-                    var pathE = Path.Combine(bupath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
+                    fn = _fileNameRegex.Replace(fn, "");
+                    var pathE = Path.Combine(dstPath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
                     var fileE = Path.Combine(pathE, fn + ".eml");
 
                     if (!File.Exists(fileE))
                     {
                         Directory.CreateDirectory(pathE);
                         var mm = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Raw);
-                        var str = _regex.Replace(mm.raw, m => m.Value == "-" ? "+" : "/");
+                        var str = _base64UrlRegex.Replace(mm.raw, m => m.Value == "-" ? "+" : "/");
                         var raw = Convert.FromBase64String(str);
                         await File.WriteAllBytesAsync(fileE, raw);
 
                         File.SetCreationTime(fileE, time.DateTime);
                         File.SetLastWriteTime(fileE, time.DateTime);
-                        File.SetCreationTime(fileJ, time.DateTime);
-                        File.SetLastWriteTime(fileJ, time.DateTime);
+                        File.SetCreationTime(jsonName, time.DateTime);
+                        File.SetLastWriteTime(jsonName, time.DateTime);
 
                         Console.Write(" Store");
                     }
