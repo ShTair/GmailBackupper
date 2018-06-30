a90ed2e674a45be8bacc632c9cb043fe8c3ffddb
ShTair <git@vsh.jp>
2018-06-30 22:42:23 +0900

    エラー処理を追加

--- GmailBackupper/Program.cs
+++ GmailBackupper/Program.cs
@@ -51,90 +51,98 @@ namespace GmailBackupper
                 var mid = await messages.GetNextMessage();
                 if (mid == null) break;
 
-                Console.Write(mid.Id);
-
-                var pathJ = Path.Combine(bupath, "json", mid.Id.Substring(14), mid.Id.Substring(12));
-                var fileJ = Path.Combine(pathJ, mid.Id + ".json");
-
-                if (!File.Exists(fileJ))
+                try
                 {
-                    Directory.CreateDirectory(pathJ);
-                    using (var stream = File.Create(fileJ))
+                    Console.Write(mid.Id);
+
+                    var pathJ = Path.Combine(bupath, "json", mid.Id.Substring(14), mid.Id.Substring(12));
+                    var fileJ = Path.Combine(pathJ, mid.Id + ".json");
+
+                    if (!File.Exists(fileJ))
                     {
-                        await gmail.GetMessageStr(mid.Id, async src =>
+                        Directory.CreateDirectory(pathJ);
+                        using (var stream = File.Create(fileJ))
                         {
-                            await src.CopyToAsync(stream);
-                        });
-                    }
-                }
-
-                var json = await File.ReadAllTextAsync(fileJ);
-                var message = JsonConvert.DeserializeObject<MessageResultModel>(json);
-                if (message == null || message.internalDate == 0)
-                {
-                    File.Delete(fileJ);
-                    Console.WriteLine();
-                    continue;
-                }
-
-                var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_jp);
-                Console.Write($" {time:yyyy-MM-dd HH:mm:ss}");
-
-                var fn = $"{time:yyyyMMddHHmmss}_{message.id}";
-                var from = message.payload.headers.FirstOrDefault(t => t.name.Equals("From", StringComparison.CurrentCultureIgnoreCase));
-                if (from != null)
-                {
-                    var m = _fromRegex.Match(from.value);
-                    if (!m.Success)
-                    {
-                        fn += "_" + from.value.Trim(20);
-                    }
-                    else
-                    {
-                        fn += "_" + (string.IsNullOrWhiteSpace(m.Groups[1].Value) ? m.Groups[2].Value : m.Groups[1].Value).Trim(20);
-                    }
-                }
-
-                var subj = message.payload.headers.FirstOrDefault(t => t.name.Equals("Subject", StringComparison.CurrentCultureIgnoreCase));
-                if (subj != null)
-                {
-                    fn += "_" + subj.value.Trim(20);
-                }
-
-                fn = _fileRegex.Replace(fn, "");
-                var pathE = Path.Combine(bupath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
-                var fileE = Path.Combine(pathE, fn + ".eml");
-
-                if (!File.Exists(fileE))
-                {
-                    Directory.CreateDirectory(pathE);
-                    var mm = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Raw);
-                    var str = _regex.Replace(mm.raw, m => m.Value == "-" ? "+" : "/");
-                    var raw = Convert.FromBase64String(str);
-                    await File.WriteAllBytesAsync(fileE, raw);
-
-                    File.SetCreationTime(fileE, time.DateTime);
-                    File.SetLastWriteTime(fileE, time.DateTime);
-                    File.SetCreationTime(fileJ, time.DateTime);
-                    File.SetLastWriteTime(fileJ, time.DateTime);
-
-                    Console.Write(" Store");
-                }
-
-                if (time < limit)
-                {
-                    message = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Minimal);
-                    if (message.labelIds.Any(t => t == "INBOX"))
-                    {
-                        if (message.labelIds.All(t => t != "STARRED"))
-                        {
-                            await gmail.MoveToTrash(mid.Id);
-                            Console.Write(" Trash");
+                            await gmail.GetMessageStr(mid.Id, async src =>
+                            {
+                                await src.CopyToAsync(stream);
+                            });
                         }
                     }
-                }
 
-                Console.WriteLine();
+                    var json = await File.ReadAllTextAsync(fileJ);
+                    var message = JsonConvert.DeserializeObject<MessageResultModel>(json);
+                    if (message == null || message.internalDate == 0)
+                    {
+                        File.Delete(fileJ);
+                        Console.WriteLine();
+                        continue;
+                    }
+
+                    var time = DateTimeOffset.FromUnixTimeMilliseconds(message.internalDate).ToOffset(_jp);
+                    Console.Write($" {time:yyyy-MM-dd HH:mm:ss}");
+
+                    var fn = $"{time:yyyyMMddHHmmss}_{message.id}";
+                    var from = message.payload.headers.FirstOrDefault(t => t.name.Equals("From", StringComparison.CurrentCultureIgnoreCase));
+                    if (from != null)
+                    {
+                        var m = _fromRegex.Match(from.value);
+                        if (!m.Success)
+                        {
+                            fn += "_" + from.value.Trim(20);
+                        }
+                        else
+                        {
+                            fn += "_" + (string.IsNullOrWhiteSpace(m.Groups[1].Value) ? m.Groups[2].Value : m.Groups[1].Value).Trim(20);
+                        }
+                    }
+
+                    var subj = message.payload.headers.FirstOrDefault(t => t.name.Equals("Subject", StringComparison.CurrentCultureIgnoreCase));
+                    if (subj != null)
+                    {
+                        fn += "_" + subj.value.Trim(20);
+                    }
+
+                    fn = _fileRegex.Replace(fn, "");
+                    var pathE = Path.Combine(bupath, time.ToString("yyyy"), time.ToString("yyyy-MM"), time.ToString("yyyy-MM-dd"));
+                    var fileE = Path.Combine(pathE, fn + ".eml");
+
+                    if (!File.Exists(fileE))
+                    {
+                        Directory.CreateDirectory(pathE);
+                        var mm = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Raw);
+                        var str = _regex.Replace(mm.raw, m => m.Value == "-" ? "+" : "/");
+                        var raw = Convert.FromBase64String(str);
+                        await File.WriteAllBytesAsync(fileE, raw);
+
+                        File.SetCreationTime(fileE, time.DateTime);
+                        File.SetLastWriteTime(fileE, time.DateTime);
+                        File.SetCreationTime(fileJ, time.DateTime);
+                        File.SetLastWriteTime(fileJ, time.DateTime);
+
+                        Console.Write(" Store");
+                    }
+
+                    if (time < limit)
+                    {
+                        message = await gmail.GetMessage(mid.Id, Gmail.MessageFormat.Minimal);
+                        if (message.labelIds.Any(t => t == "INBOX"))
+                        {
+                            if (message.labelIds.All(t => t != "STARRED"))
+                            {
+                                await gmail.MoveToTrash(mid.Id);
+                                Console.Write(" Trash");
+                            }
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(exp);
+                }
             }
         }
     }
